59aa2cb1065b99af7e97cfdc69b7ef771a84acde
John Roodt <rogamur@gmail.com>
2016-04-19 14:39:27 +0200

    Initial Commit

---
+++ My WaterTests/My_WaterTests.swift
@@ -0,0 +1,36 @@
+//
+//  My_WaterTests.swift
+//  My WaterTests
+//
+//  Created by John Roodt on 2016/04/19.
+//  Copyright © 2016 Utility Systems. All rights reserved.
+//
+
+import XCTest
+@testable import My_Water
+
+class My_WaterTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
